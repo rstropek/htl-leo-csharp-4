d25bc73be8ab81370c155571dbee25ed3d8948e8
Rainer <rainer@software-architects.at>
2020-09-24 11:10:06 +0200

    Add live coding of today's course

---
+++ live-coding/2020-09-24/02-LambdasAndCollections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+var heroes = new List<Hero>
+{
+    new("Wade", "Wilson", "Deadpool", false),
+    new(string.Empty, string.Empty, "Homelander", true),
+    new("Bruce", "Wayne", "Batman", false),
+    new(string.Empty, string.Empty, "Stormfront", true)
+};
+
+var result = Filter(heroes, h => string.IsNullOrEmpty(h.LastName));
+var heroesWhoCanFly = string.Join(", ", result);
+Console.WriteLine(heroesWhoCanFly);
+
+Filter(new[] { "Homelander", "The Deep", "Stormfront" }, hn => hn.StartsWith("H"));
+Filter(new[] { 1, 2, 3, 4, 5 }, n => n % 2 == 0);
+
+// Linq: Where
+IEnumerable<T> Filter<T>(IEnumerable<T> items, Func<T, bool> f)
+{
+    foreach (var item in items)
+    {
+        if (f(item))
+        {
+            yield return item;
+        }
+    }
+}
+
+// delegate bool Filter<T>(T h);
+
+record Hero(string FirstName, string LastName, string HeroName, bool CanFly);
---
+++ live-coding/2020-09-24/03-FuncAction/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+var watch = Stopwatch.StartNew();
+CountToNearlyInfinity(); // <<<< Method to benchmark
+watch.Stop();
+Console.WriteLine(watch.Elapsed);
+
+MeasureTime(() => CountToNearlyInfinity());
+
+Console.WriteLine($"The result is {MeasureTimeFunc(() => CalculateSomeResult())}");
+
+static void MeasureTime(Action a)
+{
+    var watch = Stopwatch.StartNew();
+    a();
+    watch.Stop();
+    Console.WriteLine(watch.Elapsed);
+}
+
+static int MeasureTimeFunc(Func<int> f)
+{
+    var watch = Stopwatch.StartNew();
+    var result = f();
+    watch.Stop();
+    Console.WriteLine(watch.Elapsed);
+    return result;
+}
+
+static void CountToNearlyInfinity()
+{
+    for (var i = 0; i < 1000000000; i++) ;
+}
+
+static int CalculateSomeResult()
+{
+    // Simulate some interesting calculation
+    for (var i = 0; i < 1000000000; i++) ;
+
+    // Return the result
+    return 42;
+}
